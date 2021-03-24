39e435114cf6b6b2fd193f85c41da0435e2cbc3e
Sinoxolo-PAL <79845766+Sinoxolo-PAL@users.noreply.github.com>
2021-03-24 07:33:11 -0700

    Changed if statement to have use OR operators

--- level09[1].js
+++ level09[1].js
@@ -4,11 +4,11 @@ function findVowels (word) {
   var vowels = ['a', 'e', 'i', 'o', 'u'];
   var vowelsInWord = [];
   for (var i = 0; i < letters.length; i++) {
-  if  (letters.includes(vowels[i]) && !(vowelsInWord.includes(letters[i]))) {
-    vowelsInWord.push(vowels[i]);
+    if (letters[i] == 'a' || letters[i] == 'e' || letters[i] == 'i' || letters[i] == 'o' || letters[i] == 'u') {
+      vowelsInWord.push(vowels[i]);
   } 
   }
-  return ("Vowels: " + vowelsInWord);
+  return "Vowels: " + vowelsInWord;
   }
   
 

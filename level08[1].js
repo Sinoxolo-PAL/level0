ad82eab58849a5bdcadc0abf5435ad3aabca89e4
Sinoxolo-PAL <79845766+Sinoxolo-PAL@users.noreply.github.com>
2021-06-08 11:41:10 +0200

    Fixed conditional statements. Added for 0 minute and hours

--- level08[1].js
+++ level08[1].js
@@ -3,14 +3,18 @@ function intToTime (int) {
   var min = int % 60;
   if (min > 1 && hours > 1) {
     return hours + " hours," + " " + min + " minutes";
-  } else if (min > 1 && hours = 1) {
+  } else if (min > 1 && hours == 1) {
     return hours + " hour," + " " + min + " minutes";
-  } else if (min = 1 && hours > 1) {
+  } else if (min == 1 && hours > 1) {
     return hours + " hours," + " " + min + " minute";
-  } else if (min = 1 && hours = 1) {
+  } else if (min == 1 && hours == 1) {
     return hours + " hour," + " " + min + " minute";
+  } else if (hours == 0 && min > 1) {
+    return min + " minutes";
+  } else if (min == 0 && hours >=1) {
+    return hours + " hours";
   }
 }
 
-console.log(intToTime(71));
-console.log(intToTime(133));
+console.log(intToTime(120));
+console.log(intToTime(134));

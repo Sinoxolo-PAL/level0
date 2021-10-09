134316763ed26de6e5043ef27ea5aa78606ae33c
Sinoxolo-PAL <79845766+Sinoxolo-PAL@users.noreply.github.com>
2021-10-09 17:13:10 +0200

    Update level08[1].js

--- level08[1].js
+++ level08[1].js
@@ -1,20 +1,24 @@
 function intToTime (int) {
-  var hours = Math.floor (int / 60);
-  var min = int % 60;
+  let hours = Math.floor (int / 60);
+  let min = int % 60;
   if (min > 1 && hours > 1) {
     return hours + " hours," + " " + min + " minutes";
-  } else if (min > 1 && hours == 1) {
+  } else if (min > 1 && hours === 1) {
     return hours + " hour," + " " + min + " minutes";
-  } else if (min == 1 && hours > 1) {
+  } else if (min === 1 && hours > 1) {
     return hours + " hours," + " " + min + " minute";
-  } else if (min == 1 && hours == 1) {
-    return hours + " hour," + " " + min + " minute";
-  } else if (hours == 0 && min > 1) {
+  } else if (min === 0 && hours === 1) {
+    return hours + " hour";
+  } else if (hours === 0 && min > 1) {
     return min + " minutes";
-  } else if (min == 0 && hours >=1) {
+  } else if (min === 0 && hours >=1) {
     return hours + " hours";
+  } else if (min === 1 && hours ===0) {
+    return min + " minute";
   }
 }
 
+console.log(intToTime(51));
+console.log(intToTime(601));
 console.log(intToTime(120));
 console.log(intToTime(134));

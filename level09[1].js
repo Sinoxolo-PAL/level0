3a1980461f44125224278d713a224ef6fba5c145
Sinoxolo-PAL <79845766+Sinoxolo-PAL@users.noreply.github.com>
2021-10-09 17:16:01 +0200

    Update level09[1].js

--- level09[1].js
+++ level09[1].js
@@ -1,11 +1,11 @@
 function findVowels (word) {
-  var lowerLetters = word.toLowerCase();
-  var letters = lowerLetters.split('');
-  var vowels = ['a', 'e', 'i', 'o', 'u'];
-  var vowelsInWord = [];
-  for (var i = 0; i < letters.length; i++) {
+  let lowerLetters = word.toLowerCase();
+  let letters = lowerLetters.split('');
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  let vowelsInWord = [];
+  for (let i = 0; i < letters.length; i++) {
     if (letters[i] == 'a' || letters[i] == 'e' || letters[i] == 'i' || letters[i] == 'o' || letters[i] == 'u') {
-      var hasVowel = vowelsInWord.includes(letters[i]);
+      let hasVowel = vowelsInWord.includes(letters[i]);
       if (hasVowel == false) {
         vowelsInWord.push(letters[i]);
         }

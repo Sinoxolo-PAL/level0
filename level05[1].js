67b9a391f44fcf85382e2cf814043c32df04af9d
Sinoxolo-PAL <79845766+Sinoxolo-PAL@users.noreply.github.com>
2021-10-13 09:13:45 +0200

    Update level05[1].js

--- level05[1].js
+++ level05[1].js
@@ -1,5 +1,5 @@
 function areaOfTriangle (b, h, s) {
- var semiPerimeter = 0.5 * (b + h + s);
- var area = Math.sqrt(semiPerimeter * ((semiPerimeter - b)*(semiPerimeter - h)*(semiPerimeter - s)))
+ let semiPerimeter = 0.5 * (b + h + s);
+ let area = Math.sqrt(semiPerimeter * ((semiPerimeter - b)*(semiPerimeter - h)*(semiPerimeter - s)))
  return area;
 }
